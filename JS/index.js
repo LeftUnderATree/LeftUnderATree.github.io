4d1d8b0322d7ea6944cc53eec6585ab022e2443a
LaikaUFO <dnblacktiger136@gmail.com>
2025-12-25 00:22:34 +0300

    FixedBen

--- JS/index.js
+++ JS/index.js
@@ -34,13 +34,6 @@ if (isLarge) {
     thumbnailImg.src = src;
     thumbnailImg.alt = alt;
 
-    // iOS Safari: reduce white flashing during fast scroll
-    thumbnailImg.loading = "eager";
-    thumbnailImg.decoding = "sync";
-    thumbnailImg.fetchPriority = "high";
-    thumbnailImg.draggable = false;
-
-
     const thumbnailTitle = document.createElement("div");
     thumbnailTitle.classList.add("thumbnail-title");
     thumbnailTitle.innerText = alt;
